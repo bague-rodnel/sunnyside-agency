5e95012268f7083fbe2afed8be6bcf46c4c356aa
Rodnel Bague <bague.rodnel@gmail.com>
2021-12-16 15:35:48 +0800

    vanila version with img mods for supporting > 1400px width screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// ============ NAVBAR POP ON SCROLL =============
 const navbar = document.querySelector(".navbar");
 
 window.onscroll = function() {
@@ -7,3 +8,26 @@ window.onscroll = function() {
     navbar.classList.remove("opaque")
   }
 }
+
+// ================= TOGGLE DROPDOWN ================
+const navbarToggler = document.querySelector(".navbar-toggler");
+const dropdownMenu = document.querySelector(".dropdownMenu");
+dropdownMenu.style.setProperty("--content-height", dropdownMenu.scrollHeight + "px")
+
+navbarToggler.onclick = function(e) {
+const showNow = dropdownMenu.dataset.show;
+
+  if (showNow === "false") {
+    dropdownMenu.dataset.show = "true";
+  } else {
+    dropdownMenu.dataset.show = "false";
+  }
+}
+
+// ==================== CLOSE MENU AT ANCHOR CLICK ===========
+const navbarLinks = document.querySelectorAll(".navbar-menu a");
+navbarLinks.forEach(link => {
+  link.onclick = function(e){ 
+    dropdownMenu.dataset.show = "false";
+  }
+})
